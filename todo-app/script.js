332adfebe25d3ba1b8b412b1cad0c1316d69bb26
Arthur Ott <arthur.ott91@web.de>
2024-08-18 12:10:38 +0200

    todo-with-api: add POST,GET and PUT request

--- todo-app/script.js
+++ todo-app/script.js
@@ -2,25 +2,28 @@ const btn = document.querySelector("button");
 const body = document.querySelector("body");
 const list = document.querySelector("#list");
 const input = document.querySelector("#input");
-
+const deleteBtn = document.querySelector("#btn-delete");
 // fetch dadta from server and log the data
 
-const api = fetch("http://localhost:4730/todos");
+const api = "http://localhost:4730/todos";
 
 let state = [];
 
 function load() {
-  api
+  state.length = 0;
+  fetch(api)
     .then((data) => {
-      console.log(data.ok);
-      console.log(data.status);
+      //console.log(data.ok);
+      //   console.log(data.status);
       return data.json();
     })
     .then((json) => {
-      state.push(json);
-      console.log(state);
+      state.push(...json);
+      //   console.log(state);
+      render();
     });
 }
+load();
 
 // create todos in the todo-Api
 let newTodoSt = {};
@@ -36,6 +39,8 @@ btn.addEventListener("click", function () {
     newTodoSt = newTodo;
     post();
     load();
+    render();
+    input.value = "";
   }
 });
 
@@ -49,10 +54,76 @@ function post() {
   })
     .then((res) => res.json())
     .then((newTodoFromApi) => {
-      // new todo from api with ID
-
-      console.log(newTodoFromApi);
+      //   console.log(newTodoFromApi);
 
       newTodoSt.length = 0;
     });
 }
+
+//render from state
+
+function render() {
+  // Clean the ul
+  list.innerHTML = "";
+
+  // Put the todos frtom state in DOM
+  state.forEach((todo) => {
+    // Create an Li Element
+    const liElement = document.createElement("li");
+
+    // To find out which checkbox clicked
+    liElement.toDoObject = todo;
+
+    // Create a checkbox for Li Element
+    const checkbox = document.createElement("input");
+    // Add a type Attribute to checkbox
+    checkbox.type = "checkbox";
+    // Add to checked Attribute a value from toDo.todos.done
+    checkbox.checked = todo.done;
+    // Add the checkbox to Li Element/liElement
+    liElement.appendChild(checkbox);
+
+    // Create an inner Text for liElement
+    const liText = document.createTextNode(todo.description);
+    // Join liText with liElement
+    liElement.append(liText);
+
+    // Add liElement to list Element
+    list.appendChild(liElement);
+  });
+}
+
+// Check if checkbox is checked
+list.addEventListener("change", (e) => {
+  const checkbox = e.target;
+  const liElement = checkbox.parentElement;
+  const todo = liElement.toDoObject;
+  todo.done = checkbox.checked;
+  const id = todo.id;
+
+  fetch("http://localhost:4730/todos/" + id, {
+    method: "PUT",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(todo),
+  })
+    .then((res) => res.json())
+    .then((updatedTodoFromApi) => {
+      // new todo from api with ID
+      //   console.log(updatedTodoFromApi);
+    });
+});
+
+//remove done todos
+deleteBtn.addEventListener("click", function () {
+  state = state.filter((todo) => todo.done === true);
+
+  state.map((todoItem) => {
+    fetch("http://localhost:4730/todos/" + todoItem.id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => {});
+  });
+  load();
+  render();
+});
